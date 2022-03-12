5338c81a776c0c471e211339b0a82e6d80646b09
Carlos Augusto <carlosferreirab25@gmail.com>
2022-03-12 17:39:23 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/Navbar/MovieCard";
 
 
 import Pagination from "components/Navbar/Pagination";
+import { BASE_URL } from "ultils/requests";
 
 function Listing() {
 
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response =>{
+            console.log(response.data);
+        });
 
     return (
         <>
